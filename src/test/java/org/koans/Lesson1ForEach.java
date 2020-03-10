3cc90ca2247f6d637d8f5544ed563d0eb7d5bda9
Llewellyn Falco <isidore@setgame.com>
2020-03-10 01:13:06 -0400

    foreach & placeholder

--- src/test/java/org/koans/Lesson1ForEach.java
+++ src/test/java/org/koans/Lesson1ForEach.java
@@ -2,9 +2,6 @@ package org.koans;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.HashMap;
-import java.util.Objects;
-
 import org.junit.Test;
 import org.koans.helpers.Koans;
 
@@ -13,74 +10,25 @@ public class Lesson1ForEach extends Koans
   @Test
   public void forEach()
   {
-    String names = ["john"]
-        
-  }
-  @Test
-  public void toNullerCase()
-  {
-    try
+    String names[] = {"Fry",
+                      "Hubert J. Farnsworth",
+                      "Bender",
+                      "Calculon",
+                      "Amy",
+                      "Hermes",
+                      "Professor Farnsworth",
+                      "Zoidberg",
+                      "Nibbler",
+                      "Zapp Brannigan",
+                      "Kif"};
+    String yKnot = "";
+    for (String name : names)
     {
-      String name = getName2();
-      String display = name.trim().toUpperCase();
+      if (name.length() == 8)
+      {
+        yKnot = name;
+      }
     }
-    catch (Exception e)
-    {
-      e.printStackTrace();
-      assertEquals(25, e.getStackTrace()[0].getLineNumber());
-    }
-  }
-  @Test
-  public void codeWithoutNulls()
-  {
-    try
-    {
-      String name = Objects.requireNonNull(getName2());
-      String display = name.trim().toUpperCase();
-    }
-    catch (Exception e)
-    {
-      e.printStackTrace();
-      assertEquals(38, e.getStackTrace()[1].getLineNumber());
-    }
-  }
-  @Test
-  public void testName() throws Exception
-  {
-    String name = "Zach";
-    int number = 42;
-    double pi = 3.14;
-    assertEquals(-1325618168, Objects.hash(name, number, pi));
-  }
-  public void testname() throws Exception
-  {
-    long debt = 100_000_000_000_000L;
-    int flags = 0b0_0011_1001;
-    HashMap<Integer, HashMap<String, Integer>> map = new HashMap<>();
-  }
-  /********************************** helper code below ********************/
-  /**
-   * 
-   * 
-   * 
-   * 
-   * 
-   * 
-   * 
-   * 
-   * 
-   * 
-   * 
-   * 
-   * 
-   * @return
-   */
-  private String getName1()
-  {
-    return "Neo";
-  }
-  private String getName2()
-  {
-    return null;
+    assertEquals(yKnot, ___);
   }
 }
---
+++ src/test/java/org/koans/Lesson2Lambdas.java
@@ -0,0 +1,19 @@
+package org.koans;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+import org.junit.Test;
+import org.koans.helpers.Koans;
+
+public class Lesson2Lambdas extends Koans
+{
+  @Test
+  public void forEach()
+  {
+    String names = ["john"]
+        
+  }
+}
